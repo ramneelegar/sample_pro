acdb1441acf08bf9b954293184f8b500a4560c12
Ramachandrappa <ramneelagar@gmail.com>
2026-09-01 22:35:39 +0530

    Add cargo and passenger classes with overridden methods

--- day05_polymorphism.java
+++ day05_polymorphism.java
@@ -21,3 +21,45 @@ class PlaneBase {
         System.out.println("Plane is landing");
     }
 }
+
+class cargo extends PlaneBase {
+    @Override
+    void takeoff() {
+        System.out.println("Plane is taking off, needs more time");
+    }
+    @Override
+    void fly() {
+        System.out.println("Cargo flies at low height");
+    }
+    @Override
+    void land() {
+        System.out.println("Cargo plane is landing safely");
+    }
+    void carryCargo() {
+        System.out.println("Cargo carries goods");
+    }
+}
+class passenger extends PlaneBase {
+    @Override
+    void takeoff() {
+        System.out.println("Plane is taking off, needs less time");
+    }
+    @Override
+    void fly() {
+        System.out.println("Passenger flies at high height");
+    }
+    @Override
+    void land() {
+        System.out.println("Passenger plane is landing safely");
+    }
+    void carryPassenger() {
+        System.out.println("Passenger carries people");
+    }
+}
+class airport {
+    void permit(PlaneBase p) {
+        p.takeoff();
+        p.fly();
+        p.land();
+    }
+}
