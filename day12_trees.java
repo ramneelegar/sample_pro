5afa6c3233f7806e70a8ffd744f0db95d0794d83
Ramachandrappa <ramneelagar@gmail.com>
2026-09-26 01:01:47 +0530

    Add height function to calculate tree height

--- day12_trees.java
+++ day12_trees.java
@@ -57,4 +57,14 @@ class Tree {
             System.out.print(root.data + " ");
         }
     }
+
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
 }
